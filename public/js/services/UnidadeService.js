8c0dbdc4db44f7de7835232f00702af4055b74b9
Fabio Lopes <fabio.lopes@gmail.com>
2017-08-06 19:27:43 -0300

    a

--- public/js/services/UnidadeService.js
+++ public/js/services/UnidadeService.js
@@ -1,8 +1,8 @@
 
 apoioApp.factory('unidadeService', function($http, $log){
 	
-	var urlPadrao = 'http://localhost:3000'; //'http://ec2-52-11-115-221.us-west-2.compute.amazonaws.com:81'
-	//var urlPadrao = 'http://ec2-35-160-247-116.us-west-2.compute.amazonaws.com:84';
+	//var urlPadrao = 'http://localhost:3000'; //'http://ec2-52-11-115-221.us-west-2.compute.amazonaws.com:81'
+	var urlPadrao = 'http://ec2-35-160-247-116.us-west-2.compute.amazonaws.com:84';
 
 	var urlAgrupamento = urlPadrao+'/api/agrupamento/v1/';
 	var urlUnidade = urlPadrao+'/api/unidade/v1/';
